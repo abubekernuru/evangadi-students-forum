3ed2cad05d3e5733d7878a7d81bb55bd25029554
abubekernuru <abubekernuru27@gmail.com>
2026-01-10 22:49:56 +0300

    create signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const User = require('../model/user.model.js')
 const bcryptjs = require('bcryptjs');
-const {errorHandler} = require('../utils/error.js')
+const {errorHandler} = require('../utils/error.js');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 
 const signup = async (req, res, next) => {
@@ -22,4 +24,30 @@ const signup = async (req, res, next) => {
     }
 }
 
-module.exports = {signup}
+const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const validUser = await User.findOne({ email });
+        if(!validUser){
+            return res.status(404).json({message: "User not found!"})
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return res.status(401).json({message: "Wrong Credential!"})
+        }
+        const token = jwt.sign({id: validUser._id,}, process.env.JWT_SECRET);
+        const {password: pass, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+        res
+            .cookie('access_token', token, {
+                httpOnly: true,
+                expires: expiryDate
+            })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {signup, signin}
