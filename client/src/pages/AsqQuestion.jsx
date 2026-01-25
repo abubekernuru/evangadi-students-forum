de7c7a76a4bbed7351f1bd5644f7d71c15acba05
abubekernuru <abubekernuru27@gmail.com>
2026-01-25 14:11:30 +0300

    start ask question page ui

--- client/src/pages/AsqQuestion.jsx
+++ client/src/pages/AsqQuestion.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function AsqQuestion() {
   return (
-    <div>AsqQuestion</div>
+    <div className='container mt-5'>
+      <h1 className='text-center font-bold mb-3'>Ask Question</h1>
+      <form className='flex flex-col gap-3'>
+        <div className='flex flex-col gap-2'>
+          <label htmlFor="questionTitle">Enter your question title: </label>
+          <input type="text" id='questionTitle' placeholder='Question title'/>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <label htmlFor="questionDetails">Enter your question details: </label>
+          <textarea type="text" id='questionDetails' placeholder='Question details' rows={6} className='w-full'></textarea>
+        </div>
+        <button type="submit">Submit Question</button>
+      </form>
+    </div>
   )
 }
 
